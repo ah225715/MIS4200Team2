4dedb163c830b180528f47a5be5c37d49df70f71
Alex Hankins <ah225715@ohio.edu>
2020-02-28 18:55:38 -0500

    Alex additional changes 2/28

---
+++ Models/userDetails.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MIS4200Team2.Models
+{
+    public class userDetails
+    {
+
+
+        [Required]
+        public Guid ID { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
+        public string firstName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Last Name")]
+        public string lastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Primary Phone")]
+        public string phoneNumber { get; set; }
+
+       
+
+    }
+    
+}
