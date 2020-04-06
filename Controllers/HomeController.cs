97ff966ec1bade5faeeb9293ea69f163f8c3c5a0
Ken Mckinley <km956216@ohio.edu>
2020-04-06 17:38:59 -0400

    update

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,16 +15,16 @@ namespace MIS4200Team2.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "At Centric we have given a lot of thought to the Corporate Character we want to exhibit. This character includes: ";
-            ViewBag.One = "1. Honesty and integrity in everything we do.";
-            ViewBag.Two = "2. We have high ethical and moral values.";
-            ViewBag.Three = "3. We are thorough. If it’s worth doing, it’s worth doing right and it deserves 100% of our attention.";
-            ViewBag.Four = "4. We have a Spartan attitude about our business. We look at every cost very carefully and ask ourselves “If I were buying this with my money, would I spend it on this?”";
-            ViewBag.Five = "5. We do what is right, not what is expedient. We do not take short - term gains at the expense of long-term values. Instead we focus on what is the right thing to do.";
-            ViewBag.Six = "6. We are humble and respect all partners. Everyone at Centric is making a vital contribution to our success. We recognize the potential in every human being and are careful to involve everyone. We want each partner to be a confident, contributing member of the team.";
-            ViewBag.Sev = "7. We look professional. We have a dress code at all our facilities and believe outward appearances say a lot about us and the company.";
-            ViewBag.Eight = "8. We are courteous. Nice people attract nice people and courtesy is contagious.";
-            ViewBag.Nine = "9. We are enthusiastic! Positive attitudes and optimism are a must in a fast-paced environment like ours. A positive outlook goes a long way.";
+            ViewBag.Message = "We’re dedicated to providing you with an unmatched experience by working together to do what’s right by you while doing what’s right for all. Our seven core values reflect our professional passions and personal convictions.";
+            ViewBag.One = "1. Commit to Delivery Excellence.";
+            ViewBag.Two = "2. Embrace Integrity and Openness";
+            ViewBag.Three = "3. Practice Responsible Stewardship";
+            ViewBag.Four = "4. Invest in an Exceptional Culture";
+            ViewBag.Five = "5. Ignite Passion for the Greater Good";
+            ViewBag.Six = "6. Strive to Innovate";
+            ViewBag.Sev = "7. Live a Balanced Life";
+            ViewBag.Eight = "";
+            ViewBag.Nine = "";
 
             return View();
         }
